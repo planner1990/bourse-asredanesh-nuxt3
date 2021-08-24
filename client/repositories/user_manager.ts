136f3cfa93bb9da17b38e10bdb6e060bba752de5
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2021-08-24 18:05:25 +0430

    correct addresses

--- client/repositories/user_manager.ts
+++ client/repositories/user_manager.ts
@@ -3,26 +3,26 @@ import { NuxtAxiosInstance } from "@nuxtjs/axios"
 
 async function getUser(userName: string | null, axios: NuxtAxiosInstance) {
   if (userName) {
-    return await axios.get('user/' + userName)
+    return await axios.get('/sso/user/' + userName)
   } else {
-    return await axios.get('user/')
+    return await axios.get('/sso/user/')
   }
 }
 
 async function getUserList(searchModel: string, axios: NuxtAxiosInstance) {
-  return await axios.post('user/list', searchModel)
+  return await axios.post('/sso/user/list', searchModel)
 }
 
 async function updateUser(form: object, axios: NuxtAxiosInstance) {
-  return await axios.post('user/update', form)
+  return await axios.post('/sso/user/update', form)
 }
 
 async function createUser(form: object, axios: NuxtAxiosInstance) {
-  return await axios.post('user/', form)
+  return await axios.post('/sso/user/', form)
 }
 
 async function deleteUser(form: object, axios: NuxtAxiosInstance) {
-  return await axios.get('user/delete/' + form)
+  return await axios.get('/sso/user/delete/' + form)
 }
 
 const userRoles: object[] = [
