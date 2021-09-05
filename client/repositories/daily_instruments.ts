3278c0ae87f8788baa19d0cfdc6a72255bb84396
Alireza Farokhi <a.farokhi@asredanesh.com>
2021-09-05 11:53:07 +0430

    resolve confilict 01

--- client/repositories/daily_instruments.ts
+++ client/repositories/daily_instruments.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 const instance = axios.create({
   withCredentials: true,
   // crossdomain: true,
-  baseURL: "http://localhost:4010",
+  baseURL: process.env.VUE_APP_Host,
   headers: {
     authorization: sessionStorage.getItem('jwtKey')
   }
