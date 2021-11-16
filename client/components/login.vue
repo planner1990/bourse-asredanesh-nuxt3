913dc63baa37861c96217b31b9fbf04cbb8edb08
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2021-11-16 16:19:48 +0330

    init new login

--- client/components/login.vue
+++ client/components/login.vue
@@ -1,85 +1,161 @@
 <template>
-  <v-card class="mx-auto mt-5" max-width="400">
-    <v-card-title>{{ $t('login.login') }}</v-card-title>
-    <v-card-text>
+  <v-card elevation="0" color="transparent" class="login">
+    <v-card-title :style="{ color: color }" class="justify-center">{{
+      $t("login.login")
+    }}</v-card-title>
+    <v-card-text
+      :style="{ color: color }"
+      class="text-center"
+    >
+      <v-divider />
       <v-text-field
+        :color="color"
         v-model="userName"
-        outlined
         :label="$t('user.username')"
-        prepend-icon="mdi-account-circle"
-        dense
+        prepend-inner-icon="mdi-account"
       >
       </v-text-field>
       <v-text-field
+        :color="color"
         v-model="password"
-        outlined
-        dense
         :label="$t('user.password')"
         :type="showPassword ? 'text' : 'Password'"
-        prepend-icon="mdi-lock"
+        prepend-inner-icon="mdi-lock"
         :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
         @click:append="showPassword = !showPassword"
         @keyup.enter="login"
+        dense
       >
       </v-text-field>
+      <v-text-field
+        :color="color"
+        v-model="userName"
+        :label="$t('user.captcha')"
+        prepend-inner-icon="mdi-qrcode"
+      ></v-text-field>
+      <v-select dense value="ایستا" :items="['ایستا', 'پویا']"> </v-select>
     </v-card-text>
     <v-card-actions>
-      <v-btn color="success" @click="login">
-        {{ $t('login.login') }}
+      <v-btn depressed color="error" @click="login" width="50%" large dark>
+        {{ $t("login.login") }}
+      </v-btn>
+      <v-btn depressed color="error" @click="register" width="50%" large dark>
+        {{ $t("login.register") }}
+      </v-btn>
+    </v-card-actions>
+    <v-divider />
+    <v-card-actions :style="{ color: color }">
+      <v-btn depressed color="transparent" @click="login" width="100%" large>
+        {{ $t("login.forget-password") }}
+      </v-btn>
+    </v-card-actions>
+    <v-divider />
+    <v-card-actions class="justify-center" :style="{ color: color }">
+      <v-btn depressed color="transparent" @click="login" >
+        <v-icon x-large>
+          mdi-apple
+        </v-icon>
+      </v-btn>
+      <v-btn depressed color="transparent" @click="login" >
+        <v-icon x-large>
+          adaico-bag-cross
+        </v-icon>
+      </v-btn>
+      <v-btn depressed color="transparent" @click="login" >
+        <v-icon x-large>
+          mdi-android
+        </v-icon>
+      </v-btn>
+      <v-btn depressed color="transparent" @click="login" >
+        <v-icon x-large>
+          mdi-microsoft
+        </v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-divider />
+    <v-card-actions class="text-center justify-center" :style="{ color: 'white' }">
+        {{ $t("login.alerts") }}
+    </v-card-actions>
+    <v-divider />
+    <v-card-actions :style="{ color: color }">
+      <v-btn depressed color="transparent" @click="login" width="100%" large>
+        {{ $t("login.about-us") }}
       </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
-import { mapActions, mapMutations } from 'vuex'
+import { mapActions, mapMutations } from "vuex";
 
 export default {
-  name: 'Login',
+  name: "Login",
   props: {
-    msg: String
+    msg: String,
+    color: {
+      type: String,
+      default: "inherit",
+    },
   },
   data: () => {
     return {
       showPassword: false,
       userName: null,
       password: null,
-      snacs: []
-    }
+      snacs: [],
+    };
   },
   methods: {
     ...mapActions({
-      dologin: 'sso/user/login',
+      dologin: "sso/user/login",
     }),
     ...mapMutations({
-      alert: 'snacks/showMessage'
+      alert: "snacks/showMessage",
     }),
-    login () {
-      const self = this
+    register() {},
+    login() {
+      const self = this;
       self
         .dologin({
           userName: self.userName,
           password: self.password,
-          axios: self.$axios
+          axios: self.$axios,
         })
         .then(
           function (data) {
             if (data >= 200 && data < 300) {
-              self.$router.push({ path: '/watchlist/' })
+              self.$router.push({ path: "/watchlist/" });
               self.alert({
-                color: 'success',
-                content: 'login.successful'
-              })
+                color: "success",
+                content: "login.successful",
+              });
             } else {
               self.alert({
-                color: 'error',
-                content: 'login.failed'
-              })
+                color: "error",
+                content: "login.failed",
+              });
             }
           },
           function (data) {}
-        )
-    }
-  }
-}
+        );
+    },
+  },
+};
 </script>
+
+<style lang="sass">
+.login
+  .v-input,
+  .theme--light
+    &.v-icon,
+    &.v-label,
+    &.v-input,
+    &.v-btn,
+    &.v-select
+      .v-select
+        &__selections
+          color: inherit
+      color: inherit
+  .theme--dark
+    color: inherit
+</style>
--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,16 +1,22 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <v-card>
-          <img alt="logo" />
+  <v-container class="login ma-0 pa-0 py-10" fluid>
+    <v-row>
+      <v-col></v-col>
+      <v-col>
+        <v-card elevation="0" color="transparent" class="text-center">
+          <img alt="logo" src="logo.png" />
         </v-card>
-      </div>
-      <div class="text-center">
-        <login />
-      </div>
-    </v-flex>
-  </v-layout>
+      </v-col>
+      <v-col></v-col>
+    </v-row>
+    <v-row>
+      <v-col md="4" sm="2"></v-col>
+      <v-col md="4" sm="8">
+        <login color="#3ea4bb" />
+      </v-col>
+      <v-col md="4" sm="2"></v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -24,3 +30,11 @@ export default defineComponent({
   setup() {},
 });
 </script>
+
+<style lang="sass" scoped>
+.login
+  width: 100%
+  overflow-x: hidden
+  overflow-y: clip
+  background-color: rgb(14, 56, 68)
+</style>
