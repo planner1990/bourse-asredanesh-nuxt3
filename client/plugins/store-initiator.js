ff6f29963ad983b98f243865830ba05d755e455e
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2021-11-17 21:10:22 +0330

    no cookie

--- client/plugins/store-initiator.js
+++ client/plugins/store-initiator.js
@@ -14,22 +14,25 @@ export default async function ({ store, req, redirect, route }) {
   else {
     ck = parse(req.headers["cookie"])
   }
+  if (ck) {
+    const refresh = ck[RefreshKey] || store.state.sso.user.refresh
+    const jwt = ck[tokenKey] || store.state.sso.user.token
+    const user = ck[userKey] || store.state.sso.user.user
 
-  const refresh = ck[RefreshKey] || store.state.sso.user.refresh
-  const jwt = ck[tokenKey] || store.state.sso.user.token
-  const user = ck[userKey] || store.state.sso.user.user
-
-  if (refresh && jwt && user) {
-    store.commit('sso/user/setRefresh', refresh)
-    store.commit('sso/user/setToken', jwt)
-    store.commit('sso/user/setUser', JSON.parse(user))
-    console.log(store.state)
-    if (route.fullPath == "/login")
-      return redirect('/')
-  } else {
-    store.commit('sso/user/logout')
-    if (needLogin(route.fullPath)) {
-      return redirect('/login')
+    if (refresh && jwt && user) {
+      store.commit('sso/user/setRefresh', refresh)
+      store.commit('sso/user/setToken', jwt)
+      store.commit('sso/user/setUser', JSON.parse(user))
+      console.log(store.state)
+      if (route.fullPath == "/login")
+        return redirect('/')
+    } else {
+      store.commit('sso/user/logout')
+      if (needLogin(route.fullPath)) {
+        return redirect('/login')
+      }
     }
+  } else {
+    return redirect('/login')
   }
 }
