c046a1839d6c64771a67020a9e42a6f1b75d9615
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2021-11-17 14:43:26 +0330

    correct auth middleware

--- client/middleware/auth.ts
+++ client/middleware/auth.ts
@@ -1,19 +1,17 @@
 import { Middleware } from '@nuxt/types'
 
 const auth: Middleware = async ({ store, route, redirect }) => {
-  const publicPages = [/\/login\/?/, /\/docs.*/, /\/?^/]
+
+  const publicPages = [/\/login\/?/, /\/docs.*/, /$\/?^/]
   if (publicPages.findIndex(item => { return item.test(route.fullPath) }) == -1) {
     let isLogin = store.getters['sso/user/isLogin']
-    if (!isLogin) {
-      await store.dispatch('sso/user/init')
-      isLogin = store.getters['sso/user/isLogin']
-    }
     if (!isLogin) {
       return redirect('/login')
     } else if (route.fullPath === '/login' && !!isLogin) {
       return redirect('/')
     }
   }
+
 }
 
 export default auth
