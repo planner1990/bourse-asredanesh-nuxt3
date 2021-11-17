419f12fdbffb340e0077e344b34c5e7b5ae54d07
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2021-11-17 15:14:48 +0330

    disable static site

--- client/middleware/auth.ts
+++ client/middleware/auth.ts
@@ -1,7 +1,6 @@
 import { Middleware } from '@nuxt/types'
 
 const auth: Middleware = async ({ store, route, redirect }) => {
-
   const publicPages = [/\/login\/?/, /\/docs.*/, /$\/?^/]
   if (publicPages.findIndex(item => { return item.test(route.fullPath) }) == -1) {
     let isLogin = store.getters['sso/user/isLogin']
