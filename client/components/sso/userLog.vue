35f98d23e9b9af0b1cff90410fdf68d0d99f55f1
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2021-12-13 13:51:39 +0330

    log time and type localization

--- client/components/sso/userLog.vue
+++ client/components/sso/userLog.vue
@@ -1,14 +1,28 @@
 <template>
-  <v-data-table :items="res" :headers="headers" dense> </v-data-table>
+  <v-data-table :items="res" :headers="headers" dense>
+    <template #item.dateTime="{ item }">
+      {{ printDate(item.dateTime) }}
+    </template>
+    <template #item.type="{ item }">
+      {{ printType(item.type) }}
+    </template>
+  </v-data-table>
 </template>
 
 <script lang="ts">
-import { defineComponent, useStore, reactive } from "@nuxtjs/composition-api";
+import {
+  defineComponent,
+  useStore,
+  reactive,
+  computed,
+} from "@nuxtjs/composition-api";
 import { Paginated, Log, WatchlistColumns } from "@/types";
+import { DateTime } from "luxon";
 
 export default defineComponent({
-  setup() {
+  setup(props, context) {
     const store = useStore();
+    const i18n = useI18n();
     const res: Array<Log> = reactive([]);
     const headers: Array<WatchlistColumns> = [
       new WatchlistColumns("dateTime", "dateTime"),
@@ -16,13 +30,30 @@ export default defineComponent({
       new WatchlistColumns("type", "type"),
     ];
     const searchParam: Paginated = new Paginated(0, 10);
+    const locale = computed(() => {
+      return store.getters["locale"];
+    });
+    function printDate(date: string) {
+      return DateTime.fromISO(date)
+        .setLocale(locale.value)
+        .toFormat(i18n.t("general.date.longdt").toString());
+    }
+    function printType(typ: string) {
+      return i18n.t("log." + typ.toLowerCase());
+    }
     store.dispatch("sso/user/getLogs", searchParam).then((resp) => {
       res.push(...resp.data.data);
     });
     return {
       res,
       headers,
+      printDate,
+      printType,
     };
+    //TODO remove in vue3
+    function useI18n() {
+      return context.root.$i18n;
+    }
   },
 });
 </script>
