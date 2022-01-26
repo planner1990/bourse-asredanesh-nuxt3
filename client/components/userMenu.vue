ea9a54c2367e6db4570cb0dd70adc00046811fe1
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2022-01-26 12:06:43 +0330

    cleanup

---
+++ client/components/userMenu.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-menu v-model="userMenu" offset-y>
+    <template #activator="{ on, attrs }">
+      <v-btn
+        height="28"
+        min-width="184px"
+        v-bind="attrs"
+        v-on="on"
+        class="ma-0 pa-0 px-3 justify-start"
+        depressed
+      >
+        <profile-picture
+          :address="currentUser.profile && currentUser.profile.profilePic"
+        />
+        <span class=".d-none .d-sm-flex">{{
+          currentUser.profile.nickname
+            ? currentUser.profile.nickname
+            : currentUser.userName
+        }}</span>
+      </v-btn>
+    </template>
+    <v-list dense>
+      <v-list-item
+        v-for="item in userMenuItems"
+        :key="item.title"
+        :to="item.to"
+        @click="
+          () => {
+            if (item.click) item.click();
+          }
+        "
+      >
+        <v-icon :color="item.color">{{ item.icon }}</v-icon>
+        <v-list-item-title :class="item.color + '--text'">
+          {{ $t(item.title) }}
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  ComputedRef,
+  defineComponent,
+  ref,
+  useRouter,
+  useStore,
+} from "@nuxtjs/composition-api";
+import ProfilePicture from "@/components/sso/profilePicture.vue";
+import { User } from "~/types";
+
+export default defineComponent({
+  components:{
+    ProfilePicture,
+  },
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+    const userMenu = ref(false);
+    const currentUser: ComputedRef<User> = computed(
+      () => store.getters["sso/user/me"] as User
+    );
+    const userMenuItems = [
+      {
+        icon: "mdi-account-cog",
+        to: "/user/",
+        title: "menu.profile",
+      },
+      {
+        icon: "mdi-logout",
+        color: "error",
+        click: doLogout,
+        title: "login.logout",
+      },
+    ];
+    function doLogout() {
+      userMenu.value = false;
+      store.dispatch("sso/user/logout");
+      router.push("/login");
+    }
+    return {
+      currentUser,
+      userMenu,
+      userMenuItems,
+    };
+  },
+});
+</script>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -69,44 +69,7 @@
         </v-badge>
       </div>
       <v-spacer />
-      <v-menu v-model="userMenu" offset-y>
-        <template #activator="{ on, attrs }">
-          <v-btn
-            height="28"
-            min-width="184px"
-            v-bind="attrs"
-            v-on="on"
-            class="ma-0 pa-0 px-3 justify-start"
-            depressed
-          >
-            <profile-picture
-              :address="currentUser.profile && currentUser.profile.profilePic"
-            />
-            <span class=".d-none .d-sm-flex">{{
-              currentUser.profile.nickname
-                ? currentUser.profile.nickname
-                : currentUser.userName
-            }}</span>
-          </v-btn>
-        </template>
-        <v-list dense>
-          <v-list-item
-            v-for="item in userMenuItems"
-            :key="item.title"
-            :to="item.to"
-            @click="
-              () => {
-                if (item.click) item.click();
-              }
-            "
-          >
-            <v-icon :color="item.color">{{ item.icon }}</v-icon>
-            <v-list-item-title :class="item.color + '--text'">
-              {{ $t(item.title) }}
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <user-menu />
     </v-app-bar>
     <v-main
       :class="{
@@ -135,10 +98,8 @@
               color="green"
               offset-y="75%"
               offset-x="-5"
-              >{{ $t("accounting.account.amount")
-              }}{{ brif.freeMoney }}</v-badge
+              >{{ $t("accounting.account.amount") }}0</v-badge
             >
-
             <v-badge
               dot
               left
@@ -146,10 +107,8 @@
               color="red"
               offset-y="75%"
               offset-x="-5"
-              >{{ $t("accounting.account.blockedAmount")
-              }}{{ brif.blockedMoney }}</v-badge
+              >{{ $t("accounting.account.blockedAmount") }}0</v-badge
             >
-
             <v-badge
               dot
               left
@@ -158,10 +117,8 @@
               offset-y="75%"
               offset-x="-5"
             >
-              {{ $t("accounting.account.onlineBlockedAmount")
-              }}{{ brif.freeMoney }}
+              {{ $t("accounting.account.onlineBlockedAmount") }}0
             </v-badge>
-
             <v-badge
               dot
               left
@@ -169,10 +126,8 @@
               color="blue"
               offset-y="75%"
               offset-x="-5"
-              >{{ $t("accounting.account.remaining")
-              }}{{ brif.freeMoney }}</v-badge
+              >{{ $t("accounting.account.remaining") }}0</v-badge
             >
-
             <v-badge
               dot
               left
@@ -180,17 +135,11 @@
               color="#89abcd"
               offset-y="75%"
               offset-x="-5"
-              >{{ $t("accounting.account.credit")
-              }}{{ brif.freeMoney }}</v-badge
+              >{{ $t("accounting.account.credit") }}0</v-badge
             >
           </span>
         </v-col>
-        <v-col cols="1.5" sm="2" class="ma-0 pa-0">
-          <v-spacer />
-          <v-btn depressed height="28" @click="() => redirect('/about-us')">
-            {{ $t("login.about-us") }}
-          </v-btn>
-        </v-col>
+        <v-col cols="1.5" sm="2" class="ma-0 pa-0"> </v-col>
       </v-row>
     </v-footer>
     <snackbar />
@@ -201,32 +150,19 @@
 import {
   defineComponent,
   computed,
-  ComputedRef,
   useStore,
   ref,
-  useRouter,
-  useContext,
 } from "@nuxtjs/composition-api";
 import snackbar from "@/components/snacks.vue";
-import ProfilePicture from "@/components/sso/profilePicture.vue";
-import { User } from "@/types";
 import { useAsrTrader } from "~/composables";
 
 export default defineComponent({
   components: {
     snackbar,
-    ProfilePicture,
   },
   setup(props, context) {
-    const router = useRouter();
     const store = useStore();
     const appManager = useAsrTrader(store);
-    const host = process.env.VUE_APP_Host;
-    const ctx = useContext();
-    const userMenu = ref(false);
-    const blockedMoney = 0;
-    const freeMoney = 0;
-    const wlEditor = ref(null);
     const rightMenu = ref({
       mini: true,
       drawer: true,
@@ -235,19 +171,6 @@ export default defineComponent({
       mini: true,
       drawer: true,
     });
-    const userMenuItems = [
-      {
-        icon: "mdi-account-cog",
-        to: "/user/",
-        title: "menu.profile",
-      },
-      {
-        icon: "mdi-logout",
-        color: "error",
-        click: doLogout,
-        title: "login.logout",
-      },
-    ];
 
     const locale = appManager.locale;
     const formatter = appManager.formatter;
@@ -255,34 +178,14 @@ export default defineComponent({
       const tab = store.getters["bottom-panel/activeTab"];
       return tab != null && tab != -1;
     });
-    const currentUser: ComputedRef<User> = computed(
-      () => store.getters["sso/user/me"] as User
-    );
-
-    function doLogout() {
-      userMenu.value = false;
-      store.dispatch("sso/user/logout");
-      router.push("/login");
-    }
 
     return {
       formatter,
       collaps,
-      host,
       locale,
-      wlEditor,
-      doLogout,
-      redirect: ctx.redirect,
-      currentUser,
-      userMenu,
-      userMenuItems,
       rightMenu,
       leftMenu,
       clipped: true,
-      brif: {
-        blockedMoney,
-        freeMoney,
-      },
     };
   },
 });
