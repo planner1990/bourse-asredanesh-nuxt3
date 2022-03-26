c40135cfddfb9519e09099af85cfcc299a2f6c43
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2022-03-26 17:12:21 +0430

    postcss does not work

--- client/pages/about-us.vue
+++ client/pages/about-us.vue
@@ -1,39 +1,12 @@
 <template>
-  <content-viewer :document="doc" />
+  <div></div>
 </template>
 
 <script lang="ts">
-import { FetchReturn } from "@nuxt/content/types/query-builder";
-import { Splitpanes, Pane } from "splitpanes";
-
-import {
-  defineComponent,
-  useContext,
-  ref,
-  Ref,
-} from "@nuxtjs/composition-api";
-import ContentViewer from "~/components/contentViewer.vue";
+import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
   layout: "public",
-  components: {
-    Splitpanes,
-    Pane,
-    ContentViewer,
-  },
-  setup(params) {
-    const ctx = useContext();
-    const doc: Ref<FetchReturn | FetchReturn[] | null> = ref(null);
-    
-    async function getContent() {
-      doc.value = await ctx.$content("about-us").fetch();
-    }
-
-    getContent();
-    return {
-      doc,
-    };
-  },
+  setup() {},
 });
 </script>
-
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,40 +1,13 @@
 <template>
-  <content-viewer :document="doc" />
+  <div></div>
 </template>
 
 <script lang="ts">
-import { FetchReturn } from "@nuxt/content/types/query-builder";
-import { Splitpanes, Pane } from "splitpanes";
-
-import {
-  defineComponent,
-  useContext,
-  ref,
-  Ref,
-} from "@nuxtjs/composition-api";
-import ContentViewer from "~/components/contentViewer.vue";
+import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
   layout: "public",
   head: {},
-  components: {
-    Splitpanes,
-    Pane,
-    ContentViewer,
-  },
-  setup(params) {
-    const ctx = useContext();
-    const doc: Ref<FetchReturn | FetchReturn[] | null> = ref(null);
-    
-    async function getContent() {
-      doc.value = await ctx.$content("index").fetch();
-    }
-
-    getContent();
-    return {
-      doc,
-    };
-  },
+  setup() {},
 });
 </script>
-
