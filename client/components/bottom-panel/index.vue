978217d90881f0260e0aa7240d58254f32f5df2d
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2022-05-16 15:40:50 +0430

    bottom panel width

--- client/components/bottom-panel/index.vue
+++ client/components/bottom-panel/index.vue
@@ -41,19 +41,19 @@ function close() {
 <style lang="postcss" scoped>
 .footer {
   @apply tw-flex tw-flex-col tw-flex-grow;
-  width: calc(100% - 96px);
   position: fixed;
   bottom: 32px;
   background-color: white;
+
   &.expanded {
-    transition: all 0.5s ease-in-out;
+    transition: height 0.5s ease-in-out;
     height: calc(100vh - 74px);
   }
+
   .header {
     @apply tw-flex tw-flex-grow tw-w-full;
     box-shadow: 0 0 12px 0 rgba(53, 84, 209, 0.05);
     align-items: center;
-    position: absolute;
     height: 32px;
     color: var(--c-primary-rgb);
     background-color: rgba(var(--c-primary), 0.05);
@@ -61,27 +61,30 @@ function close() {
     top: 0;
     left: 0;
   }
+
   .detail {
     position: relative;
     width: 100%;
     font-size: 1rem;
     line-height: 1.5;
-    height: calc(100% - 64px);
+    height: calc(100% - 32px);
     overflow-y: auto;
-    margin-top: 32px;
   }
+
   .tabs {
     @apply tw-justify-start tw-w-full;
     background-color: rgba(var(--c-default), 0.2);
     box-shadow: 0 0 1px 0 #e2e2e2;
+
     .tab {
       @apply tw-px-2;
       background-color: rgba(0, 0, 0, 0);
       border-radius: 0 !important;
     }
   }
+
   &.half {
-    transition: all 0.5s ease-in-out;
+    transition: height 0.5s ease-in-out;
     height: calc(100vh - 436px);
   }
 
@@ -92,16 +95,25 @@ function close() {
 </style>
 
 <template>
-  <footer
-    :class="{
-      footer: true,
-      expanded: expanded && tab != -1,
-      half: tab != -1 && !expanded,
-      hiden: tab == -1,
-    }"
-  >
+  <footer :class="{
+    footer: true,
+    expanded: expanded && tab != -1,
+    half: tab != -1 && !expanded,
+    hiden: tab == -1,
+  }">
     <div v-show="tab != -1" class="detail">
-      <loading :loading="showLoading" />
+      <header class="header" v-show="tab != -1">
+        <h4>
+          {{ title }}
+        </h4>
+        <ada-spacer />
+        <ada-btn class="tw-mx-[5px]" :width="24" :height="24" @click="expand()">
+          <ada-icon color="primary" :size="16"> isax-maximize-3 </ada-icon>
+        </ada-btn>
+        <ada-btn color="primary" :width="24" :height="24" @click="close()">
+          <ada-icon :size="16">isax-minus</ada-icon>
+        </ada-btn>
+      </header>
       <v-tabs-items v-model="tab" :class="{ expanded: expanded }">
         <v-tab-item>
           <default-order-list />
@@ -116,19 +128,8 @@ function close() {
           <further-information />
         </v-tab-item>
       </v-tabs-items>
+      <loading :loading="showLoading" />
     </div>
-    <header class="header" v-show="tab != -1">
-      <h4>
-        {{ title }}
-      </h4>
-      <ada-spacer />
-      <ada-btn class="tw-mx-[5px]" :width="24" :height="24" @click="expand()">
-        <ada-icon color="primary" :size="16"> isax-maximize-3 </ada-icon>
-      </ada-btn>
-      <ada-btn color="primary" :width="24" :height="24" @click="close()">
-        <ada-icon :size="16">isax-minus</ada-icon>
-      </ada-btn>
-    </header>
     <ada-toggle class="tabs" v-model="tab" :height="32" optional>
       <ada-btn class="tab" v-for="(t, i) in tabs" :key="t" :model="i">
         {{ $t(t) }}
