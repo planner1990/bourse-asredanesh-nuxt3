aef3ed559167f04a349318392669367b6c67d56f
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2022-05-25 08:34:51 +0430

    non empty watchlist

--- client/components/rightPanel/index.vue
+++ client/components/rightPanel/index.vue
@@ -49,6 +49,15 @@ const watchList: ComputedRef<Array<MenuItem>> = computed(() => {
       bookmarkPosition: BookmarkPosition.ToolBar,
     });
   }
+  if (res.length == 0) {
+    res.push({
+      icon: "isax-eye",
+      title: "new",
+      text: "new",
+      to: "/watchList/new",
+      bookmarkPosition: BookmarkPosition.ToolBar,
+    })
+  }
   return res;
 });
 const items = getMenuItems(watchList);
@@ -148,8 +157,10 @@ if (process.client) {
     top: -12px;
   }
 }
+
 .sub-item {
   position: relative;
+
   .path {
     content: "";
     top: -20px;
@@ -161,6 +172,7 @@ if (process.client) {
     position: absolute;
   }
 }
+
 .tabs {
   background-color: rgba(var(--c-primary), 0.05);
   overflow-y: auto;
@@ -168,9 +180,11 @@ if (process.client) {
   padding-bottom: 54px;
   width: 48px;
 }
+
 .tab-items {
   background-color: rgba(var(--c-primary), 0.05);
 }
+
 .details {
   overflow-y: auto;
 }
@@ -183,39 +197,49 @@ if (process.client) {
       display: block;
     }
   }
+
   &.v-navigation-drawer--mini-variant {
     width: 48px !important;
   }
+
   .v-navigation-drawer__content {
     display: flex;
     flex-direction: row;
   }
+
   ::-webkit-scrollbar {
     display: none;
   }
+
   .v-tabs-items {
     height: 100%;
     width: calc(100% - 42px);
     display: block;
+
     .v-list-item {
       border-radius: var(--border-radius-root);
     }
+
     .v-item--active {
       color: var(--c-primary-rgb);
+
       &::before {
         border-radius: var(--border-radius-root);
       }
     }
   }
+
   .v-tabs {
     width: 48px;
     vertical-align: top;
     box-shadow: -4px 0 4px rgba(0, 0, 0, 0.05);
+
     &--vertical {
-      > .v-tabs-bar {
+      >.v-tabs-bar {
         .v-tabs-bar__content {
           display: block;
         }
+
         .v-tab {
           padding: 0;
           display: block;
@@ -223,10 +247,12 @@ if (process.client) {
           justify-content: center !important;
           min-width: 48px;
           height: 32px !important;
+
           &--active {
             &::before {
               background-color: rgba(0, 0, 0, 0) !important;
             }
+
             .v-btn {
               color: var(--c-primary-rgb);
               background-color: rgba(var(--c-primary), 0.1) !important;
@@ -240,28 +266,15 @@ if (process.client) {
 </style>
 
 <template>
-  <v-navigation-drawer
-    v-model="drawer"
-    :mini-variant="mini"
-    :clipped="clipped"
-    :right="rtl"
-    class="r-panel ps-0"
-    width="256"
-    mobile-breakpoint="960"
-    fixed
-    app
-  >
+  <v-navigation-drawer v-model="drawer" :mini-variant="mini" :clipped="clipped" :right="rtl" class="r-panel ps-0"
+    width="256" mobile-breakpoint="960" fixed app>
     <v-tabs class="ma-0 pa-0" v-model="selected" vertical hide-slider center-active>
       <div class="tabs pt-1">
-        <v-tab
-          v-for="item in items"
-          :key="item.title"
-          @click="
-            () => {
-              $emit('update:mini', !mini);
-            }
-          "
-        >
+        <v-tab v-for="item in items" :key="item.title" @click="
+          () => {
+            $emit('update:mini', !mini);
+          }
+        ">
           <v-tooltip left>
             <template #activator="{ on, attrs }">
               <v-btn width="32" height="32" color="transparent" depressed>
@@ -274,16 +287,11 @@ if (process.client) {
           </v-tooltip>
         </v-tab>
         <v-divider />
-        <v-tab
-          v-for="item in shourtcuts"
-          :key="item.title"
-          :to="item.to"
-          @click="
-            () => {
-              $emit('update:mini', true);
-            }
-          "
-        >
+        <v-tab v-for="item in shourtcuts" :key="item.title" :to="item.to" @click="
+          () => {
+            $emit('update:mini', true);
+          }
+        ">
           <v-tooltip left>
             <template v-slot:activator="{ on, attrs }">
               <v-btn width="32" height="32" color="transparent" depressed>
@@ -307,26 +315,18 @@ if (process.client) {
           <v-list dense>
             <v-list-item-group>
               <div v-for="child in item.children ? item.children : []" :key="child.title">
-                <v-list-group
-                  class="item mx-1 group"
-                  v-if="child.children"
-                  append-icon.size="12"
-                >
+                <v-list-group class="item mx-1 group" v-if="child.children" append-icon.size="12">
                   <template #appendIcon>
                     <v-icon x-small> isax-arrow-down </v-icon>
                   </template>
                   <v-list-item-title slot="activator">
                     {{ child.text ? child.text : $t(child.title) }}
                   </v-list-item-title>
-                  <v-tooltip
-                    v-for="sub in child.children
-                      ? child.children.value
-                        ? child.children.value
-                        : child.children
-                      : []"
-                    :key="sub.title"
-                    left
-                  >
+                  <v-tooltip v-for="sub in child.children
+                  ? child.children.value
+                    ? child.children.value
+                    : child.children
+                  : []" :key="sub.title" left>
                     <template #activator="{ on, attrs }">
                       <div v-on="on" v-bind="attrs" class="sub-item ps-4">
                         <div class="path ms-n2"></div>
@@ -336,38 +336,22 @@ if (process.client) {
                             {{ sub.text ? sub.text : $t(sub.title) }}
                           </v-list-item-title>
                           <div class="d-flex flex-row my-0">
-                            <v-btn
-                              @click.prevent="
-                                (ev) => {
-                                  if (isMarked(sub)) unmark(sub);
-                                  else mark(sub);
-                                }
-                              "
-                              v-if="sub.bookmarkPosition"
-                              icon
-                              x-small
-                            >
-                              <v-icon
-                                :color="isMarked(sub) ? 'primary' : 'default'"
-                                x-small
-                              >
+                            <v-btn @click.prevent="
+                              (ev) => {
+                                if (isMarked(sub)) unmark(sub);
+                                else mark(sub);
+                              }
+                            " v-if="sub.bookmarkPosition" icon x-small>
+                              <v-icon :color="isMarked(sub) ? 'primary' : 'default'" x-small>
                                 isax-frame-4
                               </v-icon>
                             </v-btn>
-                            <v-btn
-                              @click.prevent="
-                                (ev) => {
-                                  setHome(sub);
-                                }
-                              "
-                              v-if="sub.bookmarkPosition"
-                              icon
-                              x-small
-                            >
-                              <v-icon
-                                :color="sub.to == home ? 'info' : 'default'"
-                                x-small
-                              >
+                            <v-btn @click.prevent="
+                              (ev) => {
+                                setHome(sub);
+                              }
+                            " v-if="sub.bookmarkPosition" icon x-small>
+                              <v-icon :color="sub.to == home ? 'info' : 'default'" x-small>
                                 isax-star-1-bold
                               </v-icon>
                             </v-btn>
@@ -380,44 +364,26 @@ if (process.client) {
                 </v-list-group>
                 <v-tooltip v-else left>
                   <template #activator="{ on, attrs }">
-                    <v-list-item
-                      v-on="on"
-                      v-bind="attrs"
-                      class="item mx-1"
-                      :to="child.to ? child.to : '#'"
-                    >
+                    <v-list-item v-on="on" v-bind="attrs" class="item mx-1" :to="child.to ? child.to : '#'">
                       <v-list-item-title>
                         {{ child.text ? child.text : $t(child.title) }}
                       </v-list-item-title>
                       <div v-if="child.to && child.to != ''" class="d-flex flex-row my-0">
-                        <v-btn
-                          @click.prevent="
-                            (ev) => {
-                              if (isMarked(child)) unmark(child);
-                              else mark(child);
-                            }
-                          "
-                          v-if="child.bookmarkPosition"
-                          icon
-                          x-small
-                        >
-                          <v-icon
-                            :color="isMarked(child) ? 'primary' : 'default'"
-                            x-small
-                          >
+                        <v-btn @click.prevent="
+                          (ev) => {
+                            if (isMarked(child)) unmark(child);
+                            else mark(child);
+                          }
+                        " v-if="child.bookmarkPosition" icon x-small>
+                          <v-icon :color="isMarked(child) ? 'primary' : 'default'" x-small>
                             isax-frame-4
                           </v-icon>
                         </v-btn>
-                        <v-btn
-                          @click.prevent="
-                            (ev) => {
-                              setHome(child);
-                            }
-                          "
-                          v-if="child.bookmarkPosition"
-                          icon
-                          x-small
-                        >
+                        <v-btn @click.prevent="
+                          (ev) => {
+                            setHome(child);
+                          }
+                        " v-if="child.bookmarkPosition" icon x-small>
                           <v-icon :color="child.to == home ? 'info' : 'default'" x-small>
                             isax-star-1-bold
                           </v-icon>
