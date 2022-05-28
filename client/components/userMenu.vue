3188f73f7f91b8a971a94c98f1a39248e61e07cc
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2022-05-28 09:17:55 +0430

    save settings

--- client/components/userMenu.vue
+++ client/components/userMenu.vue
@@ -1,85 +1,64 @@
-<template>
-  <v-menu v-model="userMenu" class="user-menu" offset-y>
-    <template #activator="{ on, attrs }">
-      <v-btn
-        height="28"
-        min-width="184px"
-        v-bind="attrs"
-        v-on="on"
-        class="ma-0 pa-0 px-3 justify-start user-menu-activator"
-        color="transparent"
-        depressed
-      >
-        <profile-picture
-          :address="currentUser.profile && currentUser.profile.profilePic"
-        />
-        <span class=".d-none .d-sm-flex">{{
-          currentUser.profile.nickname
-            ? currentUser.profile.nickname
-            : currentUser.userName
-        }}</span>
-      </v-btn>
-    </template>
-    <v-list dense>
-      <v-list-item
-        v-for="item in userMenuItems"
-        :key="item.title"
-        :to="item.to"
-        @click="
-          () => {
-            if (item.click) item.click();
-          }
-        "
-      >
-        <v-icon :color="item.color">{{ item.icon }}</v-icon>
-        <v-list-item-title :class="item.color + '--text'">
-          {{ $t(item.title) }}
-        </v-list-item-title>
-      </v-list-item>
-    </v-list>
-  </v-menu>
-</template>
-
-<script lang="ts">
-import { defineComponent, ref } from "#app";
+<script setup lang="ts">
+import { ref, computed } from "#app";
 import ProfilePicture from "@/components/sso/profilePicture.vue";
-import { MenuItem, User } from "~/types";
+import { MenuItem } from "~/types";
 import { useUser } from "~/composables";
 import { useRouter } from "#app";
+import { DateTime } from "luxon"
 
-export default defineComponent({
-  components: {
-    ProfilePicture,
+
+const router = useRouter();
+const userManager = useUser();
+const userMenu = ref(false);
+const currentUser = computed(() => userManager.me);
+const userMenuItems: Array<MenuItem> = [
+  {
+    icon: "mdi-account-cog",
+    to: "/user/",
+    title: "menu.profile",
   },
-  setup() {
-    const router = useRouter();
-    const userManager = useUser();
-    const userMenu = ref(false);
-    const currentUser = userManager.me;
-    const userMenuItems: Array<MenuItem> = [
-      {
-        icon: "mdi-account-cog",
-        to: "/user/",
-        title: "menu.profile",
-      },
-      {
-        icon: "mdi-logout",
-        color: "error",
-        click: doLogout,
-        title: "login.logout",
-      },
-    ];
-    function doLogout() {
-      userMenu.value = false;
-      userManager.doLogout();
-      router.push("/login");
-    }
-    return {
-      currentUser,
-      userMenu,
-      userMenuItems,
-    };
+  {
+    icon: "mdi-download",
+    title: "menu.saveSettings",
+    click: downloadSettings,
+    color: 'info'
   },
+  {
+    icon: "mdi-logout",
+    color: "error",
+    click: doLogout,
+    title: "login.logout",
+  },
+];
+function doLogout() {
+  userMenu.value = false;
+  userManager.doLogout();
+  router.push("/login");
+}
+function downloadSettings() {
+  const settings = URL.createObjectURL(new Blob([JSON.stringify(userManager.me.settings)], {
+    type: 'application/json'
+  }));
+  const a = document.createElement('a')
+  a.href = settings;
+  a.download = currentUser.value.userName
+    + "-settings-"
+    + DateTime.now().toISO()
+    + ".json";
+  a.dispatchEvent(
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    })
+  );
+
+}
+defineExpose({
+  ProfilePicture,
+  currentUser,
+  userMenu,
+  userMenuItems,
 });
 </script>
 
@@ -90,3 +69,32 @@ export default defineComponent({
   }
 }
 </style>
+
+<template>
+  <v-menu v-model="userMenu" class="user-menu" offset-y>
+    <template #activator="{ on, attrs }">
+      <v-btn height="28" min-width="184px" v-bind="attrs" v-on="on"
+        class="ma-0 pa-0 px-3 justify-start user-menu-activator" color="transparent" depressed>
+        <profile-picture :address="currentUser.profile && currentUser.profile.profilePic" />
+        <span class=".d-none .d-sm-flex">{{
+            currentUser.profile.nickname
+              ? currentUser.profile.nickname
+              : currentUser.userName
+        }}</span>
+      </v-btn>
+    </template>
+    <v-list dense>
+      <v-list-item v-for="item in userMenuItems" :key="item.title" :to="item.to" @click="
+        () => {
+          if (item.click) item.click();
+        }
+      ">
+        <v-icon :color="item.color">{{ item.icon }}</v-icon>
+        <v-list-item-title :class="item.color + '--text'">
+          {{ $t(item.title) }}
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
