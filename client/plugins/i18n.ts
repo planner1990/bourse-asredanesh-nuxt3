ba7fefeb8e182a2d300b2510a45087703592c094
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2022-05-29 12:07:18 +0430

    remove required from code

--- client/plugins/i18n.ts
+++ client/plugins/i18n.ts
@@ -2,7 +2,8 @@ import { Plugin } from "@nuxt/types";
 import Vue from "vue";
 import VueI18n from "vue-i18n";
 import { useAsrTrader } from "~~/composables";
-import { computed } from "#app";
+import fa from "@/locales/fa-IR";
+import en from "@/locales/en-US";
 
 Vue.use(VueI18n);
 
@@ -12,8 +13,8 @@ export default <Plugin>function ({ app, $pinia }) {
     locale: appManager.locale,
     fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE,
     messages: {
-      en: require("~/locales/en-US.json"),
-      fa: require("~/locales/fa-IR.json"),
+      en,
+      fa,
     },
   });
 };
