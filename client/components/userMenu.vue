a398a931023323cfebdfb2d7ba74423247f8ff10
mohammadhosseiniparto <m.hosseini@asredanesh.com>
2023-01-07 09:08:12 +0330

    fixed other issues

--- client/components/userMenu.vue
+++ client/components/userMenu.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import ProfilePicture from "@/components/sso/profilePicture.vue";
 import UploadableFile from "~/types/upload/UploadableFile";
-import { useUploadAbleFile } from "~~/composables";
+import {useUploadAbleFile} from "~~/composables";
 
-import { MenuItem } from "~/types";
-import { useUser } from "~/composables";
-import { DateTime } from "luxon";
+import {MenuItem} from "~/types";
+import {useUser} from "~/composables";
+import {DateTime} from "luxon";
 
 const router = useRouter();
 const userManager = useUser();
@@ -15,11 +15,12 @@ const currentUser = computed(() => userManager.me);
 const uploadFile = useUploadAbleFile();
 const files = ref<UploadableFile[]>([]);
 const content = ref(null);
+const route = useRoute();
 
 const userMenuItems: Array<MenuItem> = [
   {
     icon: "mdi-account-cog",
-    to: "/user/",
+    to: `/watchlist/${route.params.name}/settings/userInformation`,
     title: "menu.profile",
   },
   {
@@ -41,38 +42,46 @@ const userMenuItems: Array<MenuItem> = [
     title: "login.logout",
   },
 ];
+
 function doLogout() {
   userMenu.value = false;
   userManager.doLogout();
   router.push("/login");
 }
+
+function goToProfile() {
+  const route = useRoute();
+  return `/watchlist/${route.params.name}/settings/userInformation`
+}
+
 function downloadSettings() {
   const settings = URL.createObjectURL(
-    new Blob([JSON.stringify(userManager.me.settings)], {
-      type: "application/json",
-    })
+      new Blob([JSON.stringify(userManager.me.settings)], {
+        type: "application/json",
+      })
   );
   const a = document.createElement("a");
   a.href = settings;
   a.download =
-    currentUser.value.userName +
-    "-settings-" +
-    DateTime.now().toISO() +
-    ".json";
+      currentUser.value.userName +
+      "-settings-" +
+      DateTime.now().toISO() +
+      ".json";
   a.dispatchEvent(
-    new MouseEvent("click", {
-      bubbles: true,
-      cancelable: true,
-      view: window,
-    })
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+      })
   );
 }
+
 function receive_files_uploaded(e: any) {
   files.value = e.detail.value;
-  const { readerTextFile } = uploadFile;
+  const {readerTextFile} = uploadFile;
   if (
-    files.value[0].file.type.indexOf("text") == 0 ||
-    files.value[0].file.type === "application/json"
+      files.value[0].file.type.indexOf("text") == 0 ||
+      files.value[0].file.type === "application/json"
   ) {
     readerTextFile(files.value[0].file, (evt) => {
       content.value = evt.target.result;
@@ -118,21 +127,25 @@ const me = () => {
 
 .dialog-upload {
   @apply tw-w-[600px] tw-px-3;
+
   #uploader {
     @apply tw-bg-info tw-bg-opacity-5 tw-border tw-border-dotted tw-border-info tw-rounded-lg tw-mx-auto;
     @apply tw-w-11/12 tw-my-6;
     min-height: 115px;
+
     .uploader {
       @apply tw-flex tw-px-10 tw-py-5 tw-cursor-pointer;
 
       .icon {
         @apply tw-text-info tw-text-opacity-70;
       }
+
       &__title {
         &--info {
           @apply tw-text-info;
         }
       }
+
       &__description {
         @apply tw-flex tw-flex-col tw-mr-5;
 
@@ -143,15 +156,18 @@ const me = () => {
 
       &__file {
         @apply tw-relative tw-rounded tw-border tw-border-info tw-border-opacity-70 tw-flex tw-flex-col tw-items-center tw-p-2 tw-mx-1;
+
         &__close {
           @apply tw-absolute tw-left-1 tw-top-1 tw-w-7 tw-h-7 tw-bg-info tw-bg-opacity-10 hover:tw-bg-info;
           line-height: 1;
+
           &:hover {
             .icon {
               @apply tw-text-white;
             }
           }
         }
+
         &__name {
           max-width: 96px;
           min-width: 96px;
@@ -160,6 +176,7 @@ const me = () => {
       }
     }
   }
+
   footer {
     .ada-button {
       @apply tw-w-20  tw-text-white;
@@ -167,6 +184,7 @@ const me = () => {
       &:first-child {
         @apply tw-bg-success tw-bg-opacity-90 tw-mr-6;
       }
+
       &:nth-child(2) {
         @apply tw-bg-error tw-bg-opacity-90 tw-mr-3;
       }
@@ -184,20 +202,20 @@ const me = () => {
     <ada-menu :mTop="42" :mLeft="43" :active="userMenu">
       <template #activator>
         <ada-btn
-          :height="28"
-          min-width="184px"
-          class="user-menu-activator tw-py-0 tw-px-2"
-          color="transparent"
-          depressed
-          @click="userMenu = !userMenu"
-          v-ada-click-outside="() => (userMenu = false)"
+            :height="28"
+            min-width="184px"
+            class="user-menu-activator tw-py-0 tw-px-2"
+            color="transparent"
+            depressed
+            @click="userMenu = !userMenu"
+            v-ada-click-outside="() => (userMenu = false)"
         >
           <profile-picture
-            :address="currentUser.profile && currentUser.profile.profilePic"
+              :address="currentUser.profile && currentUser.profile.profilePic"
           />
           <span
-            class="tw-mr-2 tw-font-semibold"
-            v-text="
+              class="tw-mr-2 tw-font-semibold"
+              v-text="
               currentUser.profile.nickname
                 ? currentUser.profile.nickname
                 : currentUser.userName
@@ -208,24 +226,25 @@ const me = () => {
       <template #items>
         <ada-list class="tw-p-0 tw-m-0">
           <ada-list-item
-            v-for="item in userMenuItems"
-            :key="item.title"
-            :to="item.to"
-            :value="item"
-            class="hover:tw-bg-gray-200"
+              v-for="item in userMenuItems"
+              :key="item.title"
+              :to="item.to"
+              :value="item"
+              class="hover:tw-bg-gray-200"
           >
             <template #item="{ value }">
               <div
-                class="tw-cursor-pointer tw-w-full"
-                @click="
+                  class="tw-cursor-pointer tw-w-full"
+                  @click="
                   () => {
                     if (item.click) item.click();
                   }
                 "
               >
                 <ada-icon :color="value.color" :size="16">{{
-                  value.icon
-                }}</ada-icon>
+                    value.icon
+                  }}
+                </ada-icon>
                 <span v-text="$t(value.title)"></span>
               </div>
             </template>
@@ -237,60 +256,62 @@ const me = () => {
   <lazy-ada-dialog :active="triggerUploadModal">
     <div class="dialog-upload">
       <body>
-        <div
+      <div
           id="uploader"
           v-ada-drop-zone
           @uploaded_files="receive_files_uploaded"
-        >
-          <div class="uploader">
-            <TransitionGroup name="slide-fade">
-              <template v-if="!files.length">
-                <ada-icon class="uploader__icon--info" size="3.25rem"
-                  >isax-document-upload-outline</ada-icon
-                >
-                <div class="uploader__description">
-                  <h5
+      >
+        <div class="uploader">
+          <TransitionGroup name="slide-fade">
+            <template v-if="!files.length">
+              <ada-icon class="uploader__icon--info" size="3.25rem"
+              >isax-document-upload-outline
+              </ada-icon
+              >
+              <div class="uploader__description">
+                <h5
                     v-text="$t('accounting.upload')"
                     class="uploader__title--info"
-                  ></h5>
-                  <p
+                ></h5>
+                <p
                     v-text="$t('accounting.desUpload')"
                     class="uploader__description--gray"
-                  ></p>
-                </div>
-              </template>
-              <template v-else>
-                <div
+                ></p>
+              </div>
+            </template>
+            <template v-else>
+              <div
                   class="uploader__file"
                   v-for="file in files"
                   :key="file.id"
-                >
-                  <ada-btn
+              >
+                <ada-btn
                     class="uploader__file__close"
                     @click.stop="removeFile(file)"
-                  >
-                    <ada-icon size="1rem">mdi-close</ada-icon>
-                  </ada-btn>
-                  <ada-icon size="3rem" class="uploader__file__icon"
-                    >mdi-file-document</ada-icon
-                  >
-                  <span
+                >
+                  <ada-icon size="1rem">mdi-close</ada-icon>
+                </ada-btn>
+                <ada-icon size="3rem" class="uploader__file__icon"
+                >mdi-file-document
+                </ada-icon
+                >
+                <span
                     class="uploader__file__name"
                     v-text="file.file.name"
-                  ></span>
-                </div>
-              </template>
-            </TransitionGroup>
-          </div>
+                ></span>
+              </div>
+            </template>
+          </TransitionGroup>
         </div>
+      </div>
       </body>
       <!-- <p v-text="$t('instrument.remove')"></p> -->
       <footer class="tw-flex tw-items-center tw-p-2">
         <ada-btn
-          :disabled="!files.length"
-          dark
-          :width="65"
-          @click.stop.prevent="uploadSetting"
+            :disabled="!files.length"
+            dark
+            :width="65"
+            @click.stop.prevent="uploadSetting"
         >
           {{ $t("general.upload") }}
         </ada-btn>
