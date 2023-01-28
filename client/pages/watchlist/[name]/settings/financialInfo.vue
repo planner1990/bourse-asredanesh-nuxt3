912b5d06955f9bfd36397332cb483848e5766bdf
mohammadhosseiniparto <m.hosseini@asredanesh.com>
2023-01-28 13:21:01 +0330

    TG-600 Create financial info on Setting page

--- client/pages/watchlist/[name]/settings/financialInfo.vue
+++ client/pages/watchlist/[name]/settings/financialInfo.vue
@@ -47,7 +47,7 @@ function goToDeposit() {
     <div class="financialInfo">
       <div class=" tw-flex tw-items-center">
         <div class="tw-w-1/3 financialInfo_avatar">
-          <img src="../../../../assets/images/Rectangle%20493.png" alt="">
+          <img :src="require('~/assets/images/Rectangle%20493.png')" alt="">
         </div>
         <div class="tw-w-2/3 tw-pr-0 tw-mt-4">
           <div class="tw-py-0 tw-text-xl tw-text-primary tw-font-semibold">حسین لطفی</div>
