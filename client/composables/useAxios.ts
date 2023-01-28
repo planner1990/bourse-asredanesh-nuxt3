139d2ee4ee858bfc207534a9bd17d66fc52ba596
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2023-01-28 10:49:02 +0330

    init sentry

--- client/composables/useAxios.ts
+++ client/composables/useAxios.ts
@@ -1,82 +1,84 @@
-import {useRuntimeConfig} from "#app";
-import {useAsrTrader, useSnacks, useUser} from ".";
-import axios, {AxiosError, AxiosInstance} from "axios";
-import {defineStore} from "pinia";
-import {ErrorExtractor} from "~/utils/error";
+import { useRuntimeConfig } from "#app";
+import { useAsrTrader, useSnacks, useUser } from ".";
+import axios, { AxiosError, AxiosInstance } from "axios";
+import { defineStore } from "pinia";
+import { ErrorExtractor } from "~/utils/error";
+import * as Sentry from "@sentry/vue";
 
 export const useAxios = defineStore("axios", () => {
-    const appconfig = useRuntimeConfig();
-    const user = useUser();
-    const app = useAsrTrader();
-    const snacks = useSnacks();
-    let instance: null | AxiosInstance = null;
+  const appconfig = useRuntimeConfig();
+  const user = useUser();
+  const app = useAsrTrader();
+  const snacks = useSnacks();
+  let instance: null | AxiosInstance = null;
 
-    function createInstance(): AxiosInstance {
-        if (instance != null)
-            return instance;
-        instance = axios.create({});
-        instance.interceptors.request.use((config) => {
-            Object.assign(config, {
-                withCredentials: true,
-                baseURL: appconfig.public.VUE_APP_Host,
-                headers: {
-                    ...config.headers,
-                    ...{
-                        "accept-language": app.locale ?? process.env.VUE_APP_I18N_LOCALE,
-                    },
-                },
-            });
-            if (user.getToken) {
-                Object.assign(config, {
-                    headers: {
-                        ...config.headers,
-                        ...{
-                            authorization: user.getToken,
-                        },
-                    },
-                });
-            }
-            return config;
-        });
-
-        instance.interceptors.response.use(
-            (response) => {
-                return response;
+  function createInstance(): AxiosInstance {
+    if (instance != null) return instance;
+    instance = axios.create({});
+    instance.interceptors.request.use((config) => {
+      Object.assign(config, {
+        withCredentials: true,
+        baseURL: appconfig.public.VUE_APP_Host,
+        headers: {
+          ...config.headers,
+          ...{
+            "accept-language": app.locale ?? process.env.VUE_APP_I18N_LOCALE,
+          },
+        },
+      });
+      if (user.getToken) {
+        Object.assign(config, {
+          headers: {
+            ...config.headers,
+            ...{
+              authorization: user.getToken,
             },
-            async (err) => {
-                let error = ErrorExtractor(err);
-                if (error.code === 401) {
-                    //TODO Handel refresh token
-                    try {
-                        await user.refreshToken();
-                        return instance?.request(err.config);
-                    } catch (err) {
-                        error = ErrorExtractor(err as AxiosError);
-                        snacks.showMessage({
-                            content: "error." + error.code,
-                            color: "error",
-                            timeout: 1000,
-                        });
-                        if (process.client) {
-                            window.history.pushState({}, "", "/login");
-                        }
-                        // TODO serverside redirect('/login')
-                        return Promise.reject(error);
-                    }
-                } else {
-                    snacks.showMessage({
-                        content: "error." + error.code,
-                        color: "error",
-                        timeout: 1000,
-                    });
-                    return Promise.reject(error);
-                }
-            }
-        );
-        return instance;
-    }
+          },
+        });
+      }
+      return config;
+    });
 
-    return {
-        createInstance,
-    };
+    instance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      async (err) => {
+        let error = ErrorExtractor(err);
+        Sentry.captureException(err);
+        Sentry.captureMessage("What??!!");
+        if (error.code === 401) {
+          //TODO Handel refresh token
+          try {
+            await user.refreshToken();
+            return instance?.request(err.config);
+          } catch (err) {
+            error = ErrorExtractor(err as AxiosError);
+            snacks.showMessage({
+              content: "error." + error.code,
+              color: "error",
+              timeout: 1000,
+            });
+            if (process.client) {
+              window.history.pushState({}, "", "/login");
+            }
+            // TODO serverside redirect('/login')
+            return Promise.reject(error);
+          }
+        } else {
+          snacks.showMessage({
+            content: "error." + error.code,
+            color: "error",
+            timeout: 1000,
+          });
+          return Promise.reject(error);
+        }
+      }
+    );
+    return instance;
+  }
+
+  return {
+    createInstance,
+  };
 });
