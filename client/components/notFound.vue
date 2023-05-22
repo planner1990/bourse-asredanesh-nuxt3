f1b0234a187f002ab28a35bea6e926df1ca96464
mohammadhosseiniparto <m.hosseini@asredanesh.com>
2023-05-22 11:54:17 +0330

    TG-707    طرح صفحات خالی

--- client/components/notFound.vue
+++ client/components/notFound.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="tw-h-full tw-flex tw-flex-col tw-justify-center tw-items-center tw-p-3">
-        <img src="../assets/images/Group%208.png" alt="">
+        <img src="@/assets/images/notFound.png" alt="">
         <div class="tw-mt-3 tw-text-lg tw-font-bold">صفحه‌ای که دنبال آن بودید پیدا نشد!</div>
     </div>
 </template>
