1a75badfcf20631e0d23be743d624587fb24f325
mohammadhosseiniparto <m.hosseini@asredanesh.com>
2023-06-20 14:19:55 +0330

    TG-775    تغییر لوگوی عصر دانش

--- client/layouts/privateDefault.vue
+++ client/layouts/privateDefault.vue
@@ -12,18 +12,18 @@ const companyManager = useCompany();
 
 
 const rightMenu = ref({
-    mini: true,
-    drawer: true,
+  mini: true,
+  drawer: true,
 });
 const leftMenu = ref({
-    drawer: true,
+  drawer: true,
 });
 
 const lmini = computed({
-    get: () => bottomPanelManager.LeftPanelMini,
-    set(val) {
-        bottomPanelManager.LeftPanelMini = val;
-    },
+  get: () => bottomPanelManager.LeftPanelMini,
+  set(val) {
+    bottomPanelManager.LeftPanelMini = val;
+  },
 });
 
 lmini.value = true;
@@ -33,16 +33,16 @@ const collaps = computed(() => /^watchlist\/.+\/.+/.test(route.path));
 const home = computed(() => userManager.me.settings?.home);
 const clipped = ref(true);
 const showFinancInfo = computed({
-    get: () => bottomPanelManager.state.showFinancialInfo,
-    set(val) {
-        bottomPanelManager.state.showFinancialInfo = val
-    }
+  get: () => bottomPanelManager.state.showFinancialInfo,
+  set(val) {
+    bottomPanelManager.state.showFinancialInfo = val
+  }
 });
 const router = useRouter();
 const route = useRoute();
 
 function openRoute(path: string) {
-    router.push(`/watchlist/${route.params.name}/${path}`);
+  router.push(`/watchlist/${route.params.name}/${path}`);
 }
 
 
@@ -61,202 +61,202 @@ function openRoute(path: string) {
 
 <style lang="postcss" scoped>
 .hide-info {
-    @apply tw-transition-all tw-ease-in-out tw-duration-700;
-    @apply tw-bottom-[-10px] tw-absolute tw-w-[20px] tw-h-[20px] tw-right-1/2 tw-bg-primary tw-leading-[20px] tw-rounded-full;
+  @apply tw-transition-all tw-ease-in-out tw-duration-700;
+  @apply tw-bottom-[-10px] tw-absolute tw-w-[20px] tw-h-[20px] tw-right-1/2 tw-bg-primary tw-leading-[20px] tw-rounded-full;
+
+  .icon {
+    @apply tw-mb-2;
+  }
+
+  &.showFinancInfo {
+    @apply tw-bottom-[20px];
 
     .icon {
-        @apply tw-mb-2;
+      @apply tw-mb-0;
     }
-
-    &.showFinancInfo {
-        @apply tw-bottom-[20px];
-
-        .icon {
-            @apply tw-mb-0;
-        }
-    }
+  }
 }
 
 .icon {
-    @apply tw-text-white;
+  @apply tw-text-white;
 
-    &.open {
-        @apply tw-rotate-180;
-    }
+  &.open {
+    @apply tw-rotate-180;
+  }
 }
 
 .page {
-    @apply tw-relative tw-block tw-overflow-clip tw-h-screen tw-pt-[42px] tw-bg-gray4/5;
+  @apply tw-relative tw-block tw-overflow-clip tw-h-screen tw-pt-[42px] tw-bg-gray4/5;
 
-    > header {
-        @apply tw-flex tw-fixed tw-w-full tw-justify-between tw-items-center tw-align-middle;
-        top: 0;
-        left: 0;
-        height: 42px;
-        background-color: #f9fafe;
-        box-shadow: 0 1px 4px rgba(0, 0, 0, 0.05) !important;
+  > header {
+    @apply tw-flex tw-fixed tw-w-full tw-justify-between tw-items-center tw-align-middle;
+    top: 0;
+    left: 0;
+    height: 42px;
+    background-color: #f9fafe;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.05) !important;
 
-        * {
-            @apply tw-leading-[42px];
-        }
-
-        > a span {
-            @apply tw-text-primary;
-        }
-
-        div.start {
-            @apply tw-flex tw-pr-[10px];
-        }
-
-        div.center {
-            @apply tw-absolute tw-flex tw-w-full tw-justify-center;
-        }
-
-        div.end {
-            .userMenu .menu-global :deep(.ada-button) {
-                @apply tw-bg-transparent;
-            }
-        }
+    * {
+      @apply tw-leading-[42px];
     }
 
-    > main {
-        @apply tw-w-full tw-overflow-y-clip;
-        height: calc(100vh - 42px);
-        box-sizing: border-box;
-
-        &.showFinancInfo {
-            height: calc(100vh - 72px);
-        }
+    > a span {
+      @apply tw-text-primary;
     }
 
-    > .footer {
-        @apply tw-flex tw-items-center tw-w-full tw-justify-center tw-transition-all tw-ease-in-out tw-duration-700;
-        height: 0;
-        position: fixed;
-        bottom: 0;
-        overflow: clip;
-
-        &.showFinancInfo {
-            height: 32px;
-        }
-
-        .summary {
-            @apply tw-justify-self-center tw-flex tw-items-center tw-justify-center;
-
-            .badge-content {
-                min-width: 60px;
-                @apply tw-mr-1;
-            }
-
-            .ada-badge {
-                &:first-child :deep(.badge) {
-                    @apply tw-bg-success;
-                }
-
-                &:nth-child(2) :deep(.badge) {
-                    @apply tw-bg-error;
-                }
-
-                &:nth-child(3) :deep(.badge) {
-                    @apply tw-bg-warning;
-                }
-
-                &:nth-child(4) :deep(.badge) {
-                    @apply tw-bg-primary;
-                }
-
-                &:nth-child(5) :deep(.badge) {
-                    @apply tw-bg-info;
-                }
-            }
-        }
-
-        .cw {
-            @apply tw-justify-self-end tw-flex tw-text-gray3 tw-px-3;
-        }
+    div.start {
+      @apply tw-flex tw-pr-[10px];
     }
 
-    .mainBackground {
-        background-color: rgba(var(--c-default), 0.05);
-
-        &::before {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: white;
-            content: "";
-        }
+    div.center {
+      @apply tw-absolute tw-flex tw-w-full tw-justify-center;
     }
 
-    .dashboardmain-nuxt {
-        overflow-y: auto;
-        height: calc(100vh - 44px);
-        position: relative;
+    div.end {
+      .userMenu .menu-global :deep(.ada-button) {
+        @apply tw-bg-transparent;
+      }
+    }
+  }
 
-        &.showFinancInfo {
-            height: calc(100vh - 106px);
-        }
+  > main {
+    @apply tw-w-full tw-overflow-y-clip;
+    height: calc(100vh - 42px);
+    box-sizing: border-box;
 
-        &.collaps {
-            height: 382px;
-        }
+    &.showFinancInfo {
+      height: calc(100vh - 72px);
+    }
+  }
+
+  > .footer {
+    @apply tw-flex tw-items-center tw-w-full tw-justify-center tw-transition-all tw-ease-in-out tw-duration-700;
+    height: 0;
+    position: fixed;
+    bottom: 0;
+    overflow: clip;
+
+    &.showFinancInfo {
+      height: 32px;
     }
 
-    .floating-button {
-        @apply tw-fixed tw-z-[9999] tw-rounded-full tw-bg-primary tw-w-11 tw-h-11 tw-bottom-3;
+    .summary {
+      @apply tw-justify-self-center tw-flex tw-items-center tw-justify-center tw-w-10/12 tw-pr-80;
+
+      .badge-content {
+        min-width: 60px;
+        @apply tw-mr-1;
+      }
+
+      .ada-badge {
+        &:first-child :deep(.badge) {
+          @apply tw-bg-success;
+        }
+
+        &:nth-child(2) :deep(.badge) {
+          @apply tw-bg-error;
+        }
+
+        &:nth-child(3) :deep(.badge) {
+          @apply tw-bg-warning;
+        }
+
+        &:nth-child(4) :deep(.badge) {
+          @apply tw-bg-primary;
+        }
+
+        &:nth-child(5) :deep(.badge) {
+          @apply tw-bg-info;
+        }
+      }
     }
+
+    .cw {
+      @apply tw-justify-self-end tw-flex tw-text-gray3 tw-px-3 tw-w-1/6  tw-justify-end;
+    }
+  }
+
+  .mainBackground {
+    background-color: rgba(var(--c-default), 0.05);
+
+    &::before {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: white;
+      content: "";
+    }
+  }
+
+  .dashboardmain-nuxt {
+    overflow-y: auto;
+    height: calc(100vh - 44px);
+    position: relative;
+
+    &.showFinancInfo {
+      height: calc(100vh - 106px);
+    }
+
+    &.collaps {
+      height: 382px;
+    }
+  }
+
+  .floating-button {
+    @apply tw-fixed tw-z-[9999] tw-rounded-full tw-bg-primary tw-w-11 tw-h-11 tw-bottom-3;
+  }
 }
 </style>
 
 <style lang="postcss">
 .page {
-    header > .end {
-        .userMenu .ada-button {
-            @apply tw-flex tw-items-center tw-bg-transparent tw-transition-all;
+  header > .end {
+    .userMenu .ada-button {
+      @apply tw-flex tw-items-center tw-bg-transparent tw-transition-all;
 
-            > div {
-                @apply tw-flex tw-justify-center tw-items-center;
-            }
+      > div {
+        @apply tw-flex tw-justify-center tw-items-center;
+      }
 
-            &:hover {
-                @apply tw-bg-primary/5;
-            }
-        }
+      &:hover {
+        @apply tw-bg-primary/5;
+      }
     }
+  }
 
+  .dashboardmain-page {
+    @apply tw-transition-all tw-ease-in-out tw-duration-700 tw-bg-primary/5;
+    padding-left: 48px !important;
+    padding-right: 48px !important;
+  }
+
+  @media (min-width: 960px) {
     .dashboardmain-page {
-        @apply tw-transition-all tw-ease-in-out tw-duration-700 tw-bg-primary/5;
-        padding-left: 48px !important;
-        padding-right: 48px !important;
-    }
+      &.left {
+        padding-left: 256px !important;
+      }
 
-    @media (min-width: 960px) {
-        .dashboardmain-page {
-            &.left {
-                padding-left: 256px !important;
-            }
-
-            &.right {
-                padding-right: 256px !important;
-            }
-        }
+      &.right {
+        padding-right: 256px !important;
+      }
     }
+  }
 }
 </style>
 
 <template>
-    <div class="page" :class="[locale, rtl ? 'rtl' : 'ltr']">
-        <header>
-            <div class="start">
-                <nuxt-link class="tw-flex tw-px-2 tw-items-center tw-justify-center" :to="home">
-                    <img class="tw-m-0 tw-p-0 tw-h-[20px]" src="/logo.png" alt="logo"/>
-                    <span v-if="!rightMenu.mini && rightMenu.drawer" class="tw-mr-2">
+  <div class="page" :class="[locale, rtl ? 'rtl' : 'ltr']">
+    <header>
+      <div class="start">
+        <nuxt-link class="tw-flex tw-px-2 tw-items-center tw-justify-center" :to="home">
+          <img class="tw-m-0 tw-p-0 tw-h-[20px]" src="/logo.png" alt="logo"/>
+          <span v-if="!rightMenu.mini && rightMenu.drawer" class="tw-mr-2">
             {{ $t("general.proxyCompany") }}
           </span>
-                </nuxt-link>
-                <ada-icon click="click" @click.stop="
+        </nuxt-link>
+        <ada-icon click="click" @click.stop="
           () => {
             if (rightMenu.drawer) {
               rightMenu.mini = !rightMenu.mini;
@@ -273,83 +273,83 @@ function openRoute(path: string) {
   'drawer-activator',
   !rightMenu.mini && rightMenu.drawer ? 'open' : '',
 ]" :size="18">
-                    mdi-menu-open
-                </ada-icon>
-                <clock :format="$t('general.date.longdt')" class="tw-w-[220px]"/>
-            </div>
-            <div class="center">
-                <ada-badge class="tw-mx-5 tw-cursor-pointer" @click="openRoute('stockIndex')">
-                    <ada-icon :size="16" class="tw-ml-1 tw-text-success">isax-trend-up</ada-icon>
-                    <span v-text="$t('oms.bourseIndex')" class="tw-text-primary"></span>:
-                    <span class="tw-text-sm badge-content">{{ formatter.format(1502605.15) }}
+          mdi-menu-open
+        </ada-icon>
+        <clock :format="$t('general.date.longdt')" class="tw-w-[220px]"/>
+      </div>
+      <div class="center">
+        <ada-badge class="tw-mx-5 tw-cursor-pointer" @click="openRoute('stockIndex')">
+          <ada-icon :size="16" class="tw-ml-1 tw-text-success">isax-trend-up</ada-icon>
+          <span v-text="$t('oms.bourseIndex')" class="tw-text-primary"></span>:
+          <span class="tw-text-sm badge-content">{{ formatter.format(1502605.15) }}
             <span v-text="
               `(${Number(8471.09).toLocaleString(appManager.locale)})`
             " class="tw-text-error"></span>
           </span>
-                </ada-badge>
-                <ada-badge class="tw-cursor-pointer" @click="openRoute('stockIndex/otc')">
-                    <ada-icon :size="16" class="tw-ml-1 tw-text-error">isax-trend-down</ada-icon>
-                    <span v-text="$t('oms.superBourseIndex')" class="tw-text-primary"></span>:
-                    <span class="tw-text-sm badge-content">{{ formatter.format(20136.69) }}
+        </ada-badge>
+        <ada-badge class="tw-cursor-pointer" @click="openRoute('stockIndex/otc')">
+          <ada-icon :size="16" class="tw-ml-1 tw-text-error">isax-trend-down</ada-icon>
+          <span v-text="$t('oms.superBourseIndex')" class="tw-text-primary"></span>:
+          <span class="tw-text-sm badge-content">{{ formatter.format(20136.69) }}
             <span v-text="
               `(${Number(84.12).toLocaleString(appManager.locale)})`
             " class="tw-text-error"></span>
           </span>
-                </ada-badge>
-            </div>
-            <div class="end">
-                <user-menu/>
-            </div>
-        </header>
-        <right-panel v-model:mini="rightMenu.mini" v-model:clipped="clipped" v-model="rightMenu.drawer"
-                     class="shadow left"/>
-        <left-panel v-model:mini="lmini" v-model:clipped="clipped" v-model="leftMenu.drawer" class="shadow right"/>
-        <main class="dashboardmain-page" :class="{
+        </ada-badge>
+      </div>
+      <div class="end">
+        <user-menu/>
+      </div>
+    </header>
+    <right-panel v-model:mini="rightMenu.mini" v-model:clipped="clipped" v-model="rightMenu.drawer"
+                 class="shadow left"/>
+    <left-panel v-model:mini="lmini" v-model:clipped="clipped" v-model="leftMenu.drawer" class="shadow right"/>
+    <main class="dashboardmain-page" :class="{
       collaps,
       showFinancInfo,
       left: !lmini
     }">
-            <div class="dashboardmain-nuxt" :class="{ showFinancInfo }">
-                <nuxt-page/>
-            </div>
-        </main>
-        <footer class="footer dashboardmain-page" :class="{
+      <div class="dashboardmain-nuxt" :class="{ showFinancInfo }">
+        <nuxt-page/>
+      </div>
+    </main>
+    <footer class="footer dashboardmain-page" :class="{
       right: !rightMenu.mini,
       left: !lmini,
       showFinancInfo
     }">
-            <div class="summary">
-                <ada-badge class="tw-ml-3">{{ $t("accounting.account.amount") }}
-                    <span class="badge-content"> ۲٬۰۰۰٬۰۰۰</span>
-                </ada-badge>
-                <ada-badge class="tw-ml-3">{{
-                    $t("accounting.account.blockedAmount")
-                    }}<span class="badge-content">0</span></ada-badge>
-                <ada-badge class="tw-ml-3">
-                    {{ $t("accounting.account.onlineBlockedAmount") }}
-                    <span class="badge-content">0</span>
-                </ada-badge>
-                <ada-badge class="tw-ml-3">{{
-                    $t("accounting.account.remaining")
-                    }}<span class="badge-content">0</span></ada-badge>
-                <ada-badge class="tw-ml-3">{{
-                    $t("accounting.account.credit")
-                    }}<span class="badge-content">0</span></ada-badge>
-            </div>
-            <div class="cw">
-                &copy; {{ new Date().getFullYear() }} {{ $t("general.company") }}
-            </div>
-        </footer>
-        <ada-btn @click="showFinancInfo = !showFinancInfo" class="hide-info" :class="{ showFinancInfo }">
-            <ada-icon class="icon" :class="{
+      <div class="summary">
+        <ada-badge class="tw-ml-3">{{ $t("accounting.account.amount") }}
+          <span class="badge-content"> ۲٬۰۰۰٬۰۰۰</span>
+        </ada-badge>
+        <ada-badge class="tw-ml-3">
+          {{ $t("accounting.account.blockedAmount") }}
+          <span class="badge-content">0</span></ada-badge>
+        <ada-badge class="tw-ml-3">
+          {{ $t("accounting.account.onlineBlockedAmount") }}
+          <span class="badge-content">0</span>
+        </ada-badge>
+        <ada-badge class="tw-ml-3">
+          {{ $t("accounting.account.remaining") }}
+          <span class="badge-content">0</span></ada-badge>
+        <ada-badge class="tw-ml-3">
+          {{ $t("accounting.account.credit") }}
+          <span class="badge-content">0</span></ada-badge>
+      </div>
+      <div class="cw">
+        &copy; {{ new Date().getFullYear() }} {{ $t("general.company") }}
+      </div>
+    </footer>
+    <ada-btn @click="showFinancInfo = !showFinancInfo" class="hide-info" :class="{ showFinancInfo }">
+      <ada-icon class="icon" :class="{
         open: showFinancInfo
       }" :size="12">
-                isax-arrow-up-1
-            </ada-icon>
-        </ada-btn>
-        <ada-btn class="floating-button tw-right-2" @click="openRoute('chatRoom/room')">
-            <ada-icon color="white" :size="24"> isax-messages-2-bold</ada-icon>
-        </ada-btn>
-        <ada-snacks/>
-    </div>
+        isax-arrow-up-1
+      </ada-icon>
+    </ada-btn>
+    <ada-btn class="floating-button tw-right-2" @click="openRoute('chatRoom/room')">
+      <ada-icon color="white" :size="24"> isax-messages-2-bold</ada-icon>
+    </ada-btn>
+    <ada-snacks/>
+  </div>
 </template>
